c7da78c2849c66c589c29ca39137ad4c4d75a782
Omobolaji Durojaiye <99079690+OmobolajiDurojaiye@users.noreply.github.com>
2025-07-11 18:50:43 +0100

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -2,7 +2,7 @@ import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 MAIL_SERVER = "smtp.gmail.com"
